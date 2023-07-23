43f937e65b71013058767b8f50e85a0182a2e8b4
Kathir Dev <121499256+KathirRDev@users.noreply.github.com>
2023-07-23 00:47:19 -0500

    test
    test

--- src/SumOfInt.java
+++ src/SumOfInt.java
@@ -7,7 +7,7 @@ public class SumOfInt
     }
     public static void sum(int a){
         if(a==0){
-            System.out.println(value); //Prints sum after the numbers are added togethe ( if a = 0 )
+            System.out.println(value); //Prints sum after the numbers are added together ( if a = 0 )
             return;
         }
         value = value + a; // updates value to value + starting value
