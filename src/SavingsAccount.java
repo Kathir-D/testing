0a0c5eea2de8c8bd3581e5b9a57f13dfe7b85865
Kathir Dev <121499256+KathirRDev@users.noreply.github.com>
2023-07-26 15:51:47 -0500

    this going to take a while
    added help and small change

--- src/SavingsAccount.java
+++ src/SavingsAccount.java
@@ -1,4 +1,15 @@
-import java.util.*;
 class SavingsAccount extends Account {
 
 }
+
+
+
+/*
+Bank :
+Account types: savings and checking..
+A customer need to create an account in the bank..
+A customer can deposit money in it and withdraw the money.
+Interest rate for checking will be 5% for saving it is 8%.
+A customer can withdraw the money from savings account with the included interest.. min duration for collecting the interest was 1-year
+Checking account it is 2 years
+ */
